e813439cc11e0558f800c387f46ae68c077d2921
YmNIK13 <valery.stavitsky@gmail.com>
2019-03-28 01:16:01 +0200

    add module

---
+++ app/code/Ymnik/NewBlock/Controller/Index/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ymnik\NewBlock\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action {
+	
+	public function execute() {
+		echo 'Тут будут супер пылесы :)';
+		exit();
+	}
+}
